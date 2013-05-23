c38959c52b859935c8abd08f0c0c2d68db00cd67
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2013-05-23 13:37:30 +0200

    Vagrant - pierwsze proby

---
+++ vagrant/puppet/manifests/default.pp
@@ -0,0 +1,52 @@
+
+class { 'apt':
+    always_apt_update => true,
+}
+
+Exec['apt_update'] -> Package <| |>
+
+class { 'apache': }
+
+class {'apache::mod::php': }
+
+apache::vhost { 'aplikacja.lh':
+    priority      => '10',
+    port          => '80',
+    docroot       => '/vagrant/web/',
+    logroot       => '/vagrant/logs/',
+}
+
+
+class { 'git': }
+
+class { 'php-dev': }
+class { 'php-cli': }
+
+class { 'composer':
+  auto_update => true
+}
+
+
+
+
+class php-cs-fixer {
+
+  exec { 'get-php-cs-fixer':
+    command => "wget -O /usr/local/bin/php-cs-fixer http://cs.sensiolabs.org/get/php-cs-fixer.phar",
+    path    => '/usr/bin:/bin:/usr/sbin:/sbin'
+  }
+
+  exec { 'chmod-php-cs-fixer':
+    command => "chmod a+x /usr/local/bin/php-cs-fixer",
+    path    => '/usr/bin:/bin:/usr/sbin:/sbin'
+  }
+
+  exec { 'self-update-php-cs-fixer':
+    command => "/usr/local/bin/php-cs-fixer self-update",
+    path    => '/usr/bin:/bin:/usr/sbin:/sbin'
+  }
+
+}
+
+include php-cs-fixer
+
